7d541dfe9877e67f44267b804f4a5b8c24fbc09f
grey4uk <grey4uk14@gmail.com>
2020-03-15 19:16:57 +0200

    Updates

---
+++ precache-manifest.761ef73db310945113ba93b9823fcabd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1519ada2159ac655a6f23ce06c106970",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "6058c23800b325753dac",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.51c20f5a.chunk.css"
+  },
+  {
+    "revision": "5c10d95f8ebd471ee977",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.9bdf6382.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.9bdf6382.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6058c23800b325753dac",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.fc836a8b.chunk.js"
+  },
+  {
+    "revision": "4992c48e99877600891f",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.ad90512f.js"
+  }
+]);
